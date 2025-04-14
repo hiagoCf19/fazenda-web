151ef058253a50976586573763e45941f9d43db6
hiagoCf19 <hiagoferreira@id.uff.br>
2025-04-14 11:25:48 -0300

    footer and header

--- src/web/(home)/_components/footer.component.tsx
+++ src/web/(home)/_components/footer.component.tsx
@@ -1,12 +1,14 @@
 export function Footer() {
   return (
     <>
-      <footer className="md:h-[309px] h-[200px] w-full    md:p-12 md:-m-12 p-4 md:mt-12 mt-4 flex justify-center items-center space-y-4 flex-col ">
-        <img src="/full_logo.svg" />
-        <div className="flex flex-col gap-2 text-center text-zinc-600">
-          <p className="">Fazenda online LTDA</p>
-          <p>CNPJ 123456789</p>
-          <span className="text-sm">Todos os direitos reservados</span>
+      <footer className="bg-[#E4EAE7] -m-12 p-12 mt-12">
+        <div className="flex items-center justify-center flex-col gap-8">
+          <img src="/full_logo.svg" />
+          <div className="flex flex-col gap-2 text-center text-zinc-600">
+            <p className="">Fazenda online LTDA</p>
+            <p>CNPJ 123456789</p>
+            <span className="text-sm">Todos os direitos reservados</span>
+          </div>
         </div>
       </footer>
       <div className="md:hidden h-16" />
